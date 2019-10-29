a3f37af2fd80bbb18de1b83fa2279c31ac287d4c
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2019-10-29 16:21:52 -0400

    created nav tuggle

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.querySelector('h1').innerHTML = 'hello world';
+var userName = 'dom';
+document.getElementById('user').innerHTML = "Hey " + userName;
+//nav in header for toggle and display nav
+var burgerMenue = document.querySelector('.uinav');
+var nav = document.getElementsByClassName('nav');
+function openNav() {
+    nav[0].classList.toggle('closeNav');
+    document.getElementById('barone').classList.toggle('disapear');
+    document.getElementById('bartwo').classList.toggle('reverse');
+    document.getElementById('barthree').classList.toggle('reversetwo');
+}
+burgerMenue.addEventListener('click', function () {
+    openNav();
+});
--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-document.querySelector('h1').innerHTML = 'hello world';
+const userName: string = 'dom';
+
+document.getElementById('user').innerHTML = `Hey ${userName}`;
+
+//nav in header for toggle and display nav
+const burgerMenue: any = document.querySelector('.uinav');
+const nav: any = document.getElementsByClassName('nav');
+
+function openNav() {
+  nav[0].classList.toggle('closeNav')
+  document.getElementById('barone').classList.toggle('disapear');
+  document.getElementById('bartwo').classList.toggle('reverse');
+  document.getElementById('barthree').classList.toggle('reversetwo');
+}
+burgerMenue.addEventListener('click', function() {
+  openNav();
+})
+
+
+
+
+
+
+
