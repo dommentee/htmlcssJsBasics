cbc68dee1e3d1995f9bd390e62c99587046d1acf
Dom Mentee <chuku0929@DomsMacBP.fios-router.home>
2019-11-29 22:08:22 -0500

    added tab buttons

--- index.js
+++ index.js
@@ -8,39 +8,38 @@ var submitButton = document.getElementById('submit');
 //nav in header for toggle and display nav
 var burgerMenue = document.querySelector('.uinav');
 var nav = document.getElementsByClassName('nav');
-// function openNav() {
-//   if (nav[0].classList) {
-//     nav[0].classList.toggle('closeNav')
-//   } else {
-//     //IE9
-//     const classes = nav[0].className.split(" ");
-//     const i = classes.indexOf('closeNav');
-//     if (i >= 0)
-//       classes.splice(i, 1);
-//     else
-//       classes.push('closeNav');
-//       nav[0].className = classes.join(" ")
-//   }
-//   //nav[0].classList.toggle('closeNav')
-//   document.getElementById('barone').classList.toggle('disapear');
-//   document.getElementById('bartwo').classList.toggle('reverse');
-//   document.getElementById('barthree').classList.toggle('reversetwo');
-// };
-// burgerMenue.addEventListener('click', function() {
-//   openNav();
-// });
+function openNav() {
+    if (nav[0].classList) {
+        nav[0].classList.toggle('closeNav');
+    }
+    else {
+        //IE9
+        var classes = nav[0].className.split(" ");
+        var i = classes.indexOf('closeNav');
+        if (i >= 0)
+            classes.splice(i, 1);
+        else
+            classes.push('closeNav');
+        nav[0].className = classes.join(" ");
+    }
+    //nav[0].classList.toggle('closeNav')
+    document.getElementById('barone').classList.toggle('disapear');
+    document.getElementById('bartwo').classList.toggle('reverse');
+    document.getElementById('barthree').classList.toggle('reversetwo');
+}
+;
+burgerMenue.addEventListener('click', openNav);
 //inner nav
-// const teamLogo: CharacterData;
-// const userTeam: string;
-var person;
-var team;
 var handlesubmit = function () {
+    var person;
+    var team;
     if (userName != null && teamName != null) {
         person = userName.value;
         team = teamName.value;
     }
     if (!person || !team) {
         alert('enter names');
+        return;
     }
     sessionStorage.setItem(userName, person);
     user.innerHTML = " Hi " + sessionStorage.getItem(userName);
@@ -49,6 +48,4 @@ var handlesubmit = function () {
     teamName.value = ' ';
     teamLogo.innerHTML = team.charAt(0);
 };
-submitButton.addEventListener('click', function () {
-    handlesubmit();
-});
+submitButton.addEventListener('click', handlesubmit);
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 //inner nav form variables
 let teamName: any = document.getElementById('team_name');
-const teamLogo: any = document.getElementById('logo');
+let teamLogo: any = document.getElementById('logo');
 const userTeam: any = document.getElementById('user_team')
 let userName: any = document.getElementById('user_name');
 const user: any = document.getElementById('user');
@@ -10,44 +10,39 @@ const submitButton = document.getElementById('submit');
 const burgerMenue: any = document.querySelector('.uinav');
 const nav: any = document.getElementsByClassName('nav');
 
-// function openNav() {
-//   if (nav[0].classList) {
-//     nav[0].classList.toggle('closeNav')
-//   } else {
-//     //IE9
-//     const classes = nav[0].className.split(" ");
-//     const i = classes.indexOf('closeNav');
+function openNav() {
+  if (nav[0].classList) {
+    nav[0].classList.toggle('closeNav')
+  } else {
+    //IE9
+    const classes = nav[0].className.split(" ");
+    const i = classes.indexOf('closeNav');
 
-//     if (i >= 0)
-//       classes.splice(i, 1);
-//     else
-//       classes.push('closeNav');
-//       nav[0].className = classes.join(" ")
-//   }
-//   //nav[0].classList.toggle('closeNav')
-//   document.getElementById('barone').classList.toggle('disapear');
-//   document.getElementById('bartwo').classList.toggle('reverse');
-//   document.getElementById('barthree').classList.toggle('reversetwo');
-
-
-// };
-// burgerMenue.addEventListener('click', function() {
-//   openNav();
-// });
+    if (i >= 0)
+      classes.splice(i, 1);
+    else
+      classes.push('closeNav');
+      nav[0].className = classes.join(" ")
+  }
+  //nav[0].classList.toggle('closeNav')
+  document.getElementById('barone').classList.toggle('disapear');
+  document.getElementById('bartwo').classList.toggle('reverse');
+  document.getElementById('barthree').classList.toggle('reversetwo');
+};
+burgerMenue.addEventListener('click', openNav);
 
 //inner nav
-// const teamLogo: CharacterData;
-// const userTeam: string;
-
-let person: string;
-let team: string;
 const handlesubmit = () => {
+  let person: string;
+  let team: string;
+
   if (userName != null && teamName != null) {
     person = userName.value;
     team = teamName.value;
   }
   if (!person || !team ) {
     alert('enter names');
+    return
   }
   sessionStorage.setItem(userName, person)
   user.innerHTML = ` Hi ${ sessionStorage.getItem(userName)}`;
@@ -57,7 +52,5 @@ const handlesubmit = () => {
   teamName.value = ' ';
   teamLogo.innerHTML = team.charAt(0);
 };
-submitButton.addEventListener('click', function () {
-  handlesubmit();
-});
+submitButton.addEventListener('click', handlesubmit);
 
